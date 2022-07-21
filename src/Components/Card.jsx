21b47712d6b7d2c99e872f4b96fc3e13ee103ef5
meetmoradiya <gaminghawkeye13@gmail.com>
2022-07-21 14:25:18 +0530

    added changes

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,15 +1,16 @@
 import React, { useRef } from 'react'
 import { AiFillEdit, AiOutlineClose } from 'react-icons/ai'
 import { useDispatch } from 'react-redux'
-import { removeTasks } from '../common/slices/prefSlice'
+import { changeTaskColumn, removeTasks } from '../common/slices/prefSlice'
 import icon from '../../src/point.png'
 
-const Card = ({ name, taskId, columnId, index, tasks, setList,dragItem,dragOverItem }) => {
+const Card = ({ name, taskId, columnId, index, tasks, setList, dragItem, dragOverItem }) => {
     const dispatch = useDispatch()
     // const dragItem = useRef();
     // const dragOverItem = useRef();
-    const onDragStart = (e, id ,order) => {
-    console.log("🚀 ~ file: Card.jsx ~ line 8 ~ onDragStart ~ order", order);
+    const onDragStart = (e, id, order) => {
+        console.log("🚀 ~ file: Card.jsx ~ line 12 ~ onDragStart ~ id", id)
+        console.log("🚀 ~ file: Card.jsx ~ line 8 ~ onDragStart ~ order", order);
         console.log("coming heree")
         e.dataTransfer.setData("id", id);
         dragItem.current = order;
@@ -26,41 +27,37 @@ const Card = ({ name, taskId, columnId, index, tasks, setList,dragItem,dragOverI
         array[indexB] = tmp;
         return array
     }
-    const drop = (event ,e) => {
-        // let id = event.dataTransfer.getData("id");
-        let sameColumn;
-
-        // tasks.forEach(())
-    //     console.log("🚀 ~ file: Card.jsx ~ line 29 ~ drop ~ id", id)
-    // console.log("🚀 ~ file: Card.jsx ~ line 28 ~ drop ~ event", event)
-    // console.log("🚀 ~ file: Card.jsx ~ line 28 ~ drop ~ e", e,dragItem.current);
-        const copyListItems = [...tasks];
-        // console.log(copyListItems[dragItem.current],copyListItems[e]  ,"this should right")
-        const dragItemContent = copyListItems[dragItem.current];
-        copyListItems.splice(dragItem.current, 1);
-        copyListItems.splice(dragOverItem.current, 0, dragItemContent);
-        // // // console.log("🚀 ~ file: Card.jsx ~ line 29 ~ drop ~ copyListItems", copyListItems);
-        // // // console.log("🚀 ~ file: Card.jsx ~ line 32 ~ drop ~ swapElement(tasks,dragItem.current,dragOverItem.current)", swapElement(tasks, dragItem.current, dragOverItem.current));
-        setList(copyListItems);
-        dragItem.current = null;
-        dragOverItem.current = null;
+    const drop = (event) => {
+        let id = event.dataTransfer.getData("id");
+        const DraggedTask = tasks.filter((data) => data.taskId === id)
+        if (!DraggedTask.length) {
+            return dispatch(changeTaskColumn({ columnId, taskId: id }))
+        } else {
+            const copyListItems = [...tasks];
+            const dragItemContent = copyListItems[dragItem.current];
+            copyListItems.splice(dragItem.current, 1);
+            copyListItems.splice(dragOverItem.current, 0, dragItemContent);
+            setList(copyListItems);
+            dragItem.current = null;
+            dragOverItem.current = null;
+        }
     };
     return (
-        <div className="card_main" onDrop={()=>{drop(index)}} onDragEnter={(e) => dragEnter(e, index)} onDragStart={e => onDragStart(e, taskId ,index)} draggable onDragOver={(e) => { console.log() }}>
-        <div className='card_wrapper' >
-            <h1>{name}</h1>
-            
-            <div className="icon_wrapper">
+        <div className="card_main" onDrop={(e) => { drop(e) }} onDragEnter={(e) => dragEnter(e, index)} onDragStart={e => onDragStart(e, taskId, index)} draggable onDragOver={(e) => { console.log() }}>
+            <div className='card_wrapper' >
+                <h1>{name}</h1>
 
-                <AiFillEdit size={20} className='edit_icon' />
-                <AiOutlineClose onClick={() => { dispatch(removeTasks({ taskId })) }} size={20} className='close_icon' />
+                <div className="icon_wrapper">
+
+                    <AiFillEdit size={20} className='edit_icon' />
+                    <AiOutlineClose onClick={() => { dispatch(removeTasks({ taskId })) }} size={20} className='close_icon' />
+                </div>
             </div>
-        </div>
             <span className='issue_type'>WEB - LIVE ISSUE</span>
             <div className='task_id'>
                 <img src={icon} />
                 <p>SPOTS - 1037</p>
-        </div>
+            </div>
         </div>
     )
 }
--- src/Components/ColumnCard.jsx
+++ src/Components/ColumnCard.jsx
@@ -3,18 +3,19 @@ import Card from './Card'
 import { AiOutlinePlusCircle } from 'react-icons/ai'
 import { useDispatch } from 'react-redux'
 import { addTasks, changeTaskColumn, deleteColumn, editColumnName } from '../common/slices/prefSlice'
-import {  AiOutlineClose } from 'react-icons/ai'
+import { AiOutlineClose } from 'react-icons/ai'
 const ColumnCard = ({ columnId, tasks, name }) => {
     const [taskName, setTaskName] = useState("")
-    const [list , setList] = useState(tasks)
+    const [list, setList] = useState(tasks)
     const [columnName, setColumnName] = useState(name)
     const dispatch = useDispatch()
     const dragItem = useRef();
     const dragOverItem = useRef();
-    useEffect(()=>{
+    useEffect(() => {
         setList(tasks)
-    },[tasks])
-    const handleAdd = () => {
+    }, [tasks])
+    const handleAdd = (e) => {
+        e.preventDefault()
         if (taskName !== "") {
 
             dispatch(addTasks({ columnId, taskName }))
@@ -34,7 +35,7 @@ const ColumnCard = ({ columnId, tasks, name }) => {
     const onDrop = (e, cat) => {
         console.log("coming ")
         let id = e.dataTransfer.getData("id");
-        dispatch(changeTaskColumn({columnId ,taskId:id}))
+        dispatch(changeTaskColumn({ columnId, taskId: id }))
         // let tasks = this.state.tasks.filter((task) => {
         //     if (task.name == id) {
         //         task.category = cat;
@@ -52,18 +53,23 @@ const ColumnCard = ({ columnId, tasks, name }) => {
     return (
         <div className='column_card_wrapper' onDragOver={(e) => onDragOver(e)} onDrop={(e) => onDrop(e, columnId)}>
             <div className='header_inner'>
-            <h1 onBlur={handleEditColumn} onInput={(e) => { setColumnName(e.target.innerText) }} contentEditable={true}>{name}</h1>
-            <AiOutlineClose onClick={handleDeleteColumn} size={20} className='close_icon' />
+                <h1 onBlur={handleEditColumn} onInput={(e) => { setColumnName(e.target.innerText) }} contentEditable={true}>{name}</h1>
+                <AiOutlineClose onClick={handleDeleteColumn} size={20} className='close_icon' />
             </div>
-            {list.length !== 0 && list.map((data ,index) => (
+            {list.length !== 0 && list.map((data, index) => (
                 <Card dragItem={dragItem} dragOverItem={dragOverItem} name={data.taskName} setList={setList} tasks={list} index={index} taskId={data.taskId} columnId={columnId} />
             ))
             }
-            <div className='add_tasks_input'>
-                <input value={taskName} onChange={(e) => { setTaskName(e.target.value) }} className='add_tasks_input' placeholder='Add Tasks' type="text" />
-                <AiOutlinePlusCircle onClick={handleAdd} size={20} className='add_tasks' />
-            </div>
-            
+            <form onSubmit={handleAdd}>
+
+                <div className='add_tasks_input'>
+                    <input required value={taskName} onChange={(e) => { setTaskName(e.target.value) }} className='add_tasks_input' placeholder='Add Tasks' type="text" />
+                    <button type="submit">
+                        <AiOutlinePlusCircle size={20} className='add_tasks' />
+                    </button>
+                </div>
+            </form>
+
         </div>
     )
 }
